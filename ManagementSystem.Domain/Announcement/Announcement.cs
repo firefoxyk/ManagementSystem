399b08a065803099c9b889381c8c8542dcbc7ff5
Машулька Машулька <marussia01@gmail.com>
2024-05-24 13:38:55 +0300

    Исправила ошибку - Announcement это агрегат

---
+++ ManagementSystem.Domain/Announcement/Announcement.cs
@@ -0,0 +1,23 @@
+﻿using ManagementSystem.Domain.Primitives;
+using ManagementSystem.Domain.Users;
+
+namespace ManagementSystem.Domain.Announcement
+{
+    public class Announcement : AggregateRoot
+    {
+        private Announcement(AnnouncementlId announcementlId, string announcementHeadline, string announcementBody, Guid createdByUserId)
+        {
+            IdAnnouncement = announcementlId;
+            AnnouncementHeadline = announcementHeadline;
+            AnnouncementBody = announcementBody;
+            CreatedByUserId = createdByUserId;
+        }
+        private Announcement()
+        { }
+        public AnnouncementlId IdAnnouncement { get; set; }
+        public string AnnouncementHeadline { get; set; }
+        public string AnnouncementBody { get; set; }
+        public Guid CreatedByUserId { get; set; }
+        public User CreatedByUser { get; set; }
+    }
+}
