b6173e0d15101a11f63d599b59bb994a8cbce106
Машулька Машулька <marussia01@gmail.com>
2024-05-28 17:59:05 +0300

    Добавила регистрацию служб в контейнере DI

---
+++ ManagementSystem.Application/ApplicationAssemblyReference.cs
@@ -0,0 +1,9 @@
+﻿namespace ManagementSystem.Application
+{
+    using System.Reflection;
+
+    public sealed class ApplicationAssemblyReference
+    {
+        internal static readonly Assembly Assembly = typeof(ApplicationAssemblyReference).Assembly;
+    }
+}
---
+++ ManagementSystem.Application/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿
+namespace ManagementSystem.Application
+{
+    using Microsoft.Extensions.DependencyInjection;
+
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddMediatR(config =>
+            {
+                config.RegisterServicesFromAssemblyContaining<ApplicationAssemblyReference>();
+            });
+
+            return services;
+        }
+    }
+}
